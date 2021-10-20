a3252d7f5f20680bd7c038cfa5707b25bb9de52b
Anasteisha24 <anastasia.bubnova24@gmail.com>
2021-10-20 14:17:26 +0300

    add multiplication test

--- app/src/test/java/com/example/myplayground/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myplayground/ExampleUnitTest.kt
@@ -13,4 +13,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+
+    @Test
+    fun multiplication_isCorrect() {
+        assertEquals(4, 2 * 2)
+    }
 }
