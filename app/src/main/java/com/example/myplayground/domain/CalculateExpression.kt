a6fc0e87a43ea06dc822ef09069800194ff6fb59
Anasteisha24 <anastasia.bubnova24@gmail.com>
2021-11-05 23:20:21 +0300

    add NumberFormatException to CalculateExpression.kt

--- app/src/main/java/com/example/myplayground/domain/CalculateExpression.kt
+++ app/src/main/java/com/example/myplayground/domain/CalculateExpression.kt
@@ -10,32 +10,32 @@ import kotlin.math.floor
 
 fun calculateExpression(expression: String, formatResultType: FormatResultTypeEnum?): String {
 
-  if (expression.isBlank()) return ""
+  try {
+    if (expression.isBlank()) return ""
 
-  // пока последний символ не число, убираем последний элемент строки
-  var formattedExpression = expression
-  while (!formattedExpression.last().isDigit()) {
-    formattedExpression = formattedExpression.dropLast(1)
-  }
+    // пока последний символ не число, убираем последний элемент строки
+    var formattedExpression = expression
+    while (!formattedExpression.last().isDigit()) {
+      formattedExpression = formattedExpression.dropLast(1)
+    }
 
-//  if (!formattedExpression.isEmpty()) {
-//    formattedExpression = formattedExpression
-//  }
+    val solution = DoubleEvaluator().evaluate(formattedExpression)
 
-  val solution = DoubleEvaluator().evaluate(formattedExpression)
+    // TODO: 05.11.2021 DigitsAfterPoint
+    val result = when (formatResultType) {
+      FormatResultTypeEnum.ONE -> String.format("%.1f", solution).toDouble()
+      FormatResultTypeEnum.TWO -> String.format("%.2f", solution).toDouble()
+      FormatResultTypeEnum.TREE -> String.format("%.3f", solution).toDouble()
+      FormatResultTypeEnum.MANY -> solution
+      null -> solution
+    }
 
-  // TODO: 05.11.2021 DigitsAfterPoint 
-  val result = when (formatResultType) {
-    FormatResultTypeEnum.ONE -> String.format("%.1f", solution).toDouble()
-    FormatResultTypeEnum.TWO -> String.format("%.2f", solution).toDouble()
-    FormatResultTypeEnum.TREE -> String.format("%.3f", solution).toDouble()
-    FormatResultTypeEnum.MANY -> solution
-    null -> solution
-  }
-
-  return if (floor(result) == result) {
-    result.toInt().toString()
-  } else {
-    result.toString()
+    return if (floor(result) == result) {
+      result.toInt().toString()
+    } else {
+      result.toString()
+    }
+  } catch (exc: NumberFormatException) {
+    return "Ошибка"
   }
 }
